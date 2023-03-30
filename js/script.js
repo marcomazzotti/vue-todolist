252913f389b77c996bc6aa097664e5bc1c0a3a1a
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-03-30 17:34:43 +0200

    Add: Milestone 1 and Milestone 2

--- js/script.js
+++ js/script.js
@@ -3,8 +3,42 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-      
-    }
+      todos: [
+        {
+          todo: "Fare la spesa",
+          done: false,
+        },
+        {
+          todo: "Andare dal meccanico",
+          done: false,
+        },
+        {
+          todo: "Fare i compiti",
+          done: false,
+        }
+      ],
+      newTodo: "",
+      error: false
+    };
+  },
+  methods: {
+    addTodo(){
+      if(this.newTodo.trim() === ""){
+        this.error = true;
+        return
+      }
+
+      this.todos.push({
+        todo: this.newTodo.charAt(0).toUpperCase() + this.newTodo.slice(1),
+                done: false,
+      });
+
+      this.newTodo = "";
+      this.error = false;
+    },
+    deleteTodo(indexToRemove) {
+      this.todos[indexToRemove].done = true
+    } 
   }
 
-}).mount("@app")
+}).mount("#app")
